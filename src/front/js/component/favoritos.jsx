c70886ab4a91bb5381a0623051bd01877e4f3b0b
Dfacundorod <112908434+Dfacundorod@users.noreply.github.com>
2024-11-04 19:25:53 +0000

    acomodando funcionalidades y layout

--- src/front/js/component/favoritos.jsx
+++ src/front/js/component/favoritos.jsx
@@ -10,24 +10,30 @@ export const Favoritos = () => {
         actions.loadFavs()
       },[]);
 
+	
+
 	return(
 		<div className="text-center mt-5">
-			<h1>Favorios! </h1>
-			
+			<h1>Favoritos!</h1>
 			<div className="d-flex justify-content-center col flex-column">
-				{store.favoritos.map((favorito, index) => (
-					<div key={index} className="m-1 rounded w-75 p-3 border border-info row d-flex align-items-center col mx-auto">
-						
-						<div className="col">
-							<h3>Id receta: {favorito.recipe_id} </h3>
+				{store.favoritos && store.favoritos.length>0 ? (
+					store.favoritos.map((favorito, index)=>(
+						<div key={index} className="m-1 rounded w-75 p-3 border border-info row d-flex align-items-center col mx-auto">
+							
+							<div className="col">
+								<h3>Id receta: {favorito.recipe_id}</h3>
+							</div>
+	
+							<div className="col p-auto">
+								<button type="button" className="btn btn-danger" onClick={()=>actions.deleteFav(index)}>Eliminar</button>
+							</div>
 						</div>
-
-						<div className="col p-auto">
-							<button type="button" class="btn btn-danger" onClick={()=>actions.deleteFav(index)}>Eliminar</button>
-						</div>
-					</div>
-				))}
+					))
+				):(
+					<p>No hay favoritos para mostrar.</p>
+				)}
 			</div>
 		</div>
 	);
+	
 };
--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -4,7 +4,6 @@ import { Link, useNavigate } from "react-router-dom";
 
 export const Navbar = () => {
 	const [isLogin, setIsLogin]=useState(false)
-	const [categories, setCategories] = useState([])
 	const [search, setSearch] = useState("")
 	const navigate = useNavigate()
 	useEffect(()=>{
@@ -12,12 +11,7 @@ export const Navbar = () => {
 		if(token){
 			setIsLogin(true)
 		} else{ setIsLogin(false)}
-		fetch(process.env.BACKEND_URL + '/api/categorias',{ 
-			method: 'GET', 
-			headers: { 'Content-Type': 'application/json' } 
-		}) 
-		.then(response=> response.json()) 
-		.then(data=> setCategories(data));
+
 	},[localStorage.getItem('token')])
 	const logOut = ()=>{
 		localStorage.removeItem('token');
@@ -42,27 +36,20 @@ export const Navbar = () => {
 						<input className="form-control me-2" type="search" placeholder="Buscar recetas" aria-label="Search" value={search} onChange={(e)=>setSearch(e.target.value)}/>
 						<button className="btn btn-outline-success" type="submit">Buscar</button>
 					</form>
-					<div className="nav-item dropdown">
-						<a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">Categorías</a>
-						<ul className="dropdown-menu" aria-labelledby="navbarDropdown">
-							{categories.map((category) => (
-								<li key={category.id}> 
-									<Link className="dropdown-item" to={`/category/search/${category.id}`}>{category.name}</Link>
-								</li>))} 
-						</ul>
-					</div>
 						{(isLogin) ? <>
 							<Link className="mx-2" to={"/recipe/create"} ><button className="btn btn-primary">Crear Receta</button></Link>
 							<Link className="mx-2" to={"/favoritos"} ><button className="btn btn-warning">Favoritos</button></Link>
 							<button onClick={logOut} className="btn btn-danger">LogOut</button>
 						</> : 
-							(<Link to="/login/cocinero">
-							<button className="btn btn-primary">Login Cocinero</button>
-							</Link>)
+							(<>
+							<Link to="/login/cocinero">
+							<button className="btn btn-primary mx-2">Login Cocinero</button>
+							</Link>
+							<Link to="/login/administrador">
+							<button className="btn btn-primary">login admin</button>
+			   				</Link>
+							</>)
 						}
-				<Link to="/login/administrador">
-				<button className="btn btn-primary">login admin</button>
-			   </Link>
 				</div>
 			</div>
 		</nav>
